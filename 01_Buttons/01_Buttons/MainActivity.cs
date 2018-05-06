eb4b817f1017d2c3ded6bef21f9d283bf88f41f9
Amir J <amir.jahan@gmail.com>
2018-05-06 12:40:39 -0700

    Up

---
+++ 01_Buttons/01_Buttons/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace _Buttons
+{
+    [Activity(Label = "01_Buttons", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        void ButtonClickedAction (object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, "Hello", ToastLength.Long).Show();
+        }
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button tempButton = FindViewById<Button>(Resource.Id.);
+
+            tempButton.Click += ButtonClickedAction;
+        }
+    }
+}
+
