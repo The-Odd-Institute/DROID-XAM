712b5ab140999cdbbef07d014ca79c4964cb5f05
Sanjay Shah <Sanjay Shah>
2018-05-09 13:56:01 -0700

    basic radio group and navigation

---
+++ NavAndSegment/MainActivity.cs
@@ -0,0 +1,98 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Views;
+using Android.Views.Animations;
+
+namespace NavAndSegment
+{
+    [Activity(Label = "Nav+Segment", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+       
+
+
+        RadioGroup mapListRadioGroup;
+        RadioButton mapButton;
+        RadioButton listButton;
+
+        TextView tempLabel;
+        Button navigateButton;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Window.RequestFeature(WindowFeatures.NoTitle);
+
+            SetContentView(Resource.Layout.Main);
+
+
+
+            Toolbar topToolbar = FindViewById<Toolbar>(Resource.Id.topToolbar);
+            Button backButton = FindViewById<Button>(Resource.Id.backButton);
+
+            navigateButton = FindViewById<Button>(Resource.Id.navigateButton);
+            navigateButton.Click += NavigateButton_Click;
+
+            tempLabel = FindViewById<TextView>(Resource.Id.tempTextView);
+
+
+            CreateSegmentControl();
+
+
+        }
+
+
+
+
+        void NavigateButton_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(SecondActivity));
+
+            StartActivity(intent);
+
+            OverridePendingTransition(Resource.Animation.slide_from_right, Resource.Animation.slide_to_left);
+
+
+        }
+
+
+        void CreateSegmentControl()
+        {
+            mapListRadioGroup = FindViewById<RadioGroup>(Resource.Id.mapListRadioGroup);
+            mapButton = FindViewById<RadioButton>(Resource.Id.mapButton);
+            listButton = FindViewById<RadioButton>(Resource.Id.listButton);
+
+			mapButton.Checked = true;
+
+            mapListRadioGroup.CheckedChange += MapListRadioGroup_CheckedChanged;
+
+
+
+        }
+
+
+        void MapListRadioGroup_CheckedChanged(object sender, RadioGroup.CheckedChangeEventArgs e)
+        {
+            //bring in map/list
+            if (listButton.Checked)
+            {
+                tempLabel.Text = "In ListView";
+            }
+
+            else if (mapButton.Checked)
+            {
+                tempLabel.Text = "In MapView";
+            }
+
+         
+   
+
+        }
+
+    }
+
+}
+
---
+++ NavAndSegment/SecondActivity.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Views.Animations;
+
+namespace NavAndSegment
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+
+		Button backButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+			Window.RequestFeature(WindowFeatures.NoTitle);
+
+			SetContentView(Resource.Layout.Second);
+            
+            Toolbar topToolbar = FindViewById<Toolbar>(Resource.Id.topToolbar);
+            backButton = FindViewById<Button>(Resource.Id.backButton);
+
+			backButton.Click += BackButton_Click;
+           
+        }
+
+		void BackButton_Click(object sender, EventArgs e)
+		{
+            
+			Finish();
+
+    
+			OverridePendingTransition(Resource.Animation.slide_from_left, Resource.Animation.slide_to_right);
+		}
+
+    }
+}
