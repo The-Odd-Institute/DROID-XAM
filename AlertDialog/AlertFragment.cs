9c5392a4a1691dc84c3a465f678ba5ecaf571434
Sanjay Shah <Sanjay Shah>
2018-05-12 16:52:16 -0700

    added alert fragment app

---
+++ AlertDialog/AlertFragment.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AlertDialog
+{
+
+    //sends the property data back to the activity class
+	public class OnClickEventArgs: EventArgs
+	{
+
+		private string propertyNumber;
+
+		public string PropertyNumber {
+
+			get { return propertyNumber; }
+			set { propertyNumber = value; }
+			
+		}
+
+		public OnClickEventArgs(string propertyNumber): base()
+		{
+			PropertyNumber = propertyNumber;
+		}
+		
+	}
+   
+
+	public class AlertFragment : Fragment
+    {
+
+		public event EventHandler<OnClickEventArgs> GoToProperty;
+
+		public override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);         
+		}
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+           
+			View view =  inflater.Inflate(Resource.Layout.Alert, container, false);
+
+			RelativeLayout relativeLayout = view.FindViewById<RelativeLayout>(Resource.Id.RelativeLayout);
+			relativeLayout.Click += RelativeLayout_Click;
+
+			TextView priceLabel = view.FindViewById<TextView>(Resource.Id.priceLabel);
+			TextView sizeLabel = view.FindViewById<TextView>(Resource.Id.sizeLabel);
+			TextView bedsLabel = view.FindViewById<TextView>(Resource.Id.bedsLabel);
+			TextView bathsLabel = view.FindViewById<TextView>(Resource.Id.bathsLabel);
+            
+			Button cancelButton = view.FindViewById<Button>(Resource.Id.cancelButton);
+            cancelButton.Click += CancelButton_Click;
+          
+
+			return view;
+
+        }
+
+		void CancelButton_Click(object sender, System.EventArgs e)
+        {
+
+			Console.WriteLine("Cancel");
+
+			FragmentManager.BeginTransaction()
+						   .Remove(this).Commit();
+
+        }
+
+		void RelativeLayout_Click(object sender, EventArgs e)
+		{
+
+			GoToProperty.Invoke(this, new OnClickEventArgs("property1"));
+
+			FragmentManager.BeginTransaction()
+                           .Remove(this).Commit();
+			
+		}
+       
+    }
+}
---
+++ AlertDialog/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+using Java.IO;
+
+namespace AlertDialog
+{
+    [Activity(Label = "AlertDialog", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+      
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button showAlertButton = FindViewById<Button>(Resource.Id.showAlert);
+            showAlertButton.Click += ShowAlertButton_Click;
+        }
+
+
+
+		void ShowAlertButton_Click(object sender, System.EventArgs e)
+		{
+            
+			AlertFragment fragment = new AlertFragment();
+
+			FragmentManager.BeginTransaction()
+			               .Add(Resource.Id.fragmentContainer, fragment, "frag")
+			  .SetCustomAnimations(Resource.Animation.abc_fade_in, Resource.Animation.abc_fade_out)
+			  // .Show(fragment)
+			  .Commit();
+
+			fragment.GoToProperty += Fragment_GoToProperty;
+         
+		}
+
+		void Fragment_GoToProperty(object sender, OnClickEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine(e.PropertyNumber);
+		}
+
+
+ 
+    }
+}
+
