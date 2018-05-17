d578542f446429cf25ec6a6c07340c59a320d722
Sanjay Shah <Sanjay Shah>
2018-05-17 13:36:18 -0700

    added keyboard dismissal

---
+++ KeyboardDismissal/MainActivity.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views.InputMethods;
+using Android.Support.V4.View;
+using static Android.Views.View;
+using Android.Views;
+
+namespace KeyboardDismissal
+{
+    [Activity(Label = "KeyboardDismissal", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity, IOnFocusChangeListener
+    {
+             
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+         
+            Button button = FindViewById<Button>(Resource.Id.button);
+			button.Click += Button_Click;
+            
+			EditText bottom = FindViewById<EditText>(Resource.Id.editText1);
+			bottom.OnFocusChangeListener = this;
+
+
+			EditText top = FindViewById<EditText>(Resource.Id.editText2);
+			top.OnFocusChangeListener = this;
+             
+        }
+        
+		void Button_Click(object sender, System.EventArgs e)
+        {
+			HideSoftKeyBoard((View) sender);
+        }
+
+      
+
+
+		public void OnFocusChange(View v, bool hasFocus)
+        {
+			if (!hasFocus) {
+				HideSoftKeyBoard(v);
+            }
+        }
+
+
+
+		private void HideSoftKeyBoard(View view)
+        {
+			InputMethodManager imm = (InputMethodManager)GetSystemService(InputMethodService);
+
+			if (imm.IsAcceptingText)
+            { // verify if the soft keyboard is open                      
+				imm.HideSoftInputFromWindow(view.WindowToken, 0);
+            }
+        }
+        
+
+              
+      
+	}
+}
+
---
+++ KeyboardDismissal/obj/Debug/android/src/md529cac9e3f0b90c8f53da929f3d7b0787/MainActivity.java
@@ -0,0 +1,57 @@
+package md529cac9e3f0b90c8f53da929f3d7b0787;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer,
+		android.view.View.OnFocusChangeListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onFocusChange:(Landroid/view/View;Z)V:GetOnFocusChange_Landroid_view_View_ZHandler:Android.Views.View/IOnFocusChangeListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("KeyboardDismissal.MainActivity, KeyboardDismissal", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("KeyboardDismissal.MainActivity, KeyboardDismissal", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onFocusChange (android.view.View p0, boolean p1)
+	{
+		n_onFocusChange (p0, p1);
+	}
+
+	private native void n_onFocusChange (android.view.View p0, boolean p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
