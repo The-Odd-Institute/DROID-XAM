10f1b60c833baa155667e5a82c56c426b1d323b4
Sanjay Shah <Sanjay Shah>
2018-05-21 12:30:08 -0700

    delegation

---
+++ Delegation/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace Delegation
+{
+    [Activity(Label = "Delegation", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+    {
+      
+
+		TextView textView;
+
+
+
+		protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+			button.Click += Button_Click;
+
+			textView = FindViewById<TextView>(Resource.Id.textView1);
+			textView.Text = "2nd activity Button hasnt been clicked yet";
+
+        }
+
+		void Button_Click(object sender, System.EventArgs e)
+		{
+          
+			Intent intent = new Intent(this, typeof(SecondActivity));
+
+			StartActivityForResult(intent, 0);
+ 
+           
+		}
+
+		public void UpdateText(int clicks)
+        {
+            textView.Text = "2nd activity's button has been clicked: " + clicks + " times";
+        }
+
+
+
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (resultCode == Result.Ok)
+            {
+				int intRetFromResult = data.GetIntExtra("clicks", 0);
+				UpdateText(intRetFromResult);
+            }
+        }
+
+    }
+}
+
---
+++ Delegation/SecondActivity.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Delegation
+{
+    
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+
+		int count = 0;
+  
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Second);
+
+
+			Button clickButton = FindViewById<Button>(Resource.Id.button1);
+			clickButton.Click += Button_Click;
+
+			Button backButton = FindViewById<Button>(Resource.Id.button2);
+			backButton.Click += BackButton_Click;
+        }
+
+		void Button_Click(object sender, EventArgs e)
+		{
+            
+			count++;
+		}
+        
+        void BackButton_Click(object sender, EventArgs e)
+		{
+         
+            Intent returnIntent = new Intent(this, typeof(MainActivity));
+            returnIntent.PutExtra("clicks", count);
+            SetResult(Result.Ok, returnIntent);
+            Finish();
+
+		}
+
+    }
+}
